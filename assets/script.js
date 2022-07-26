e86b49ef994295aee416107f91f8b9b3bc38014d
LizRanZim <lrzclass22@gmail.com>
2022-07-26 18:07:19 -0400

    Set up code for basic quiz without styling

---
+++ assets/script.js
@@ -0,0 +1,136 @@
+// Psuedo code
+// Define 3 boxes to hold start button, hold timer, hold quiz
+// function that I want to happen when start button is clicked
+// if else for each question that describes what happens when question is right, else what happens if question is wrong
+// need to write the questions
+
+
+// Question array to hold questions and answers
+var questionArr = [
+  {
+    question: "what color is the ocean?",
+    choices: ['yellow', 'purple', 'pink', 'blue'],
+    correctAnswer: 'blue'
+  },
+  {
+    question: "what color is the apple?",
+    choices: ['black', 'purple', 'green', 'blue'],
+    correctAnswer: 'green'
+  }
+]
+
+// Selects element by id
+var timeEl = document.getElementById("time");
+var startBtn = document.getElementById("startBtn");
+var quizScreen = document.getElementById("quizScreen")
+var choiceEl = document.getElementById('choices')
+var quizStatus = document.getElementById('status')
+
+// sets time to take the quiz
+var secondsLeft = 51;
+
+// sets the question index
+var questionIndex = 0;
+
+
+// function to control the timer with the quiz
+function setTime() {
+  // Sets interval in variable
+  console.log('working')
+
+  var startScreen = document.getElementById('startScreen');
+
+  startScreen.setAttribute('class', 'hide');
+
+  quizScreen.removeAttribute('class')
+
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      //  Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+
+  getQuestions();
+
+}
+
+// gets questions for quiz
+function getQuestions() {
+
+  // assigns the current question from the questions array
+  var currentQuest = questionArr[questionIndex]
+  //console.log(currentQuest.question)
+
+  // displays current question in question id in html
+  var questionEl = document.getElementById('question');
+  questionEl.textContent = currentQuest.question;
+
+  // clears text content for the choices after the question is answered
+  choiceEl.textContent = "";
+
+  // creates buttons for each of the choices so user can click on them
+  currentQuest.choices.forEach(function (choice, i) {
+    // console.log(choice)
+    var choiceBtn = document.createElement('button');
+    choiceBtn.setAttribute('class', 'choice');
+    choiceBtn.setAttribute('value', choice)
+
+    choiceBtn.textContent = choice;
+
+    // on click runs function choiceClick
+    choiceBtn.onclick = choiceClick;
+
+    // appends choice button to choice element
+    choiceEl.appendChild(choiceBtn)
+
+
+  })
+}
+
+
+function choiceClick() {
+  //console.log('clicked', this.value);
+
+
+  // if answer is wrong decrement timer by 5 seconds, tell them they are wrong
+  if (this.value !== questionArr[questionIndex].correctAnswer) {
+    console.log('wrong');
+    secondsLeft -= 5;
+    timeEl.textContent = secondsLeft;
+    quizStatus.textContent = 'Try Again';
+  }
+
+  //  if answer is right, tell them they are right
+  else {
+    console.log('right')
+    quizStatus.textContent = 'You are right.';
+  }
+
+  // moves onto the next question in the question array by adding to the question index
+  questionIndex++;
+
+
+  // looks to see if there are more questions to see if the question index matches the question array length
+
+  if (questionIndex === questionArr.length) {
+    //need to add code to call a fucntion to end the quiz
+    console.log('end quiz')
+  } else {
+    // code that calls the get Questions function if there are more questions
+    getQuestions();
+  }
+
+}
+
+
+
+startBtn.addEventListener("click", setTime)
+
+//TO DO : create a function to end the quiz and display users score and put that in local storage
